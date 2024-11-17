199d64f87e97bc4da8a92d85514822f2810e6937
Kh. Radwan <optimisticfellow3652@gmail.com>
2024-11-17 22:14:51 +0600

    Delete myproject directory

--- myproject/mosquitt/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MosquittConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mosquitt'
--- myproject/mosquitt/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Feature(models.Model):
-    name = models.CharField(max_length=50)
-    details = models.CharField(max_length=500)
